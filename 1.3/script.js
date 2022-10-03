aad353c07f59753427679c9cf6a522f155e24183
MarcoSpinaBZZ <spinam@bzz.ch>
2022-10-03 09:50:52 +0200

    Aufgabe 1.2

---
+++ 1.3/script.js
@@ -0,0 +1,8 @@
+alert("Hello World");
+
+const source = document.getElementById("source");
+const textContentOutput = document.getElementById("textContentOutput");
+const innerTextOutput = document.getElementById("innerTextOutput");
+
+textContentOutput.value = source.textContent;
+innerTextOutput.value = source.innerText;
