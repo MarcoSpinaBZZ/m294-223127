e8f5c666518ae3186912a9399678c90639f5cc52
MarcoSpinaBZZ <spinam@bzz.ch>
2022-10-03 10:46:24 +0200

    feat: add 1.3

--- 1.3/script.js
+++ 1.3/script.js
@@ -1,8 +1,1 @@
 alert("Hello World");
-
-const source = document.getElementById("source");
-const textContentOutput = document.getElementById("textContentOutput");
-const innerTextOutput = document.getElementById("innerTextOutput");
-
-textContentOutput.value = source.textContent;
-innerTextOutput.value = source.innerText;
